5fd04a9d261de0cb4600be6370bb1fd11327e404
edgard2001 <76737347+edgard2001@users.noreply.github.com>
2021-01-03 23:18:02 +0000

    Fixed enemy marker bug

--- Space Game/Assets/Scripts/HUD/ScreenMarkers.cs
+++ Space Game/Assets/Scripts/HUD/ScreenMarkers.cs
@@ -15,15 +15,23 @@ public class ScreenMarkers : MonoBehaviour
         foreach (GameObject enemy in enemies)
         {
             Vector3 markerPosition = cam.WorldToScreenPoint(enemy.transform.position);
+
             markerPosition.y = Screen.height - markerPosition.y;
-            markerPosition.x = Mathf.Clamp(markerPosition.x, 15, Screen.width - 15);
-            markerPosition.y = Mathf.Clamp(markerPosition.y, 15, Screen.height - 15);
+
             if (markerPosition.z < 0)
             {
-
+                if (markerPosition.x < Screen.width/2)
+                {
+                    markerPosition.x = Screen.width;
+                }
+                else
+                {
+                    markerPosition.x = 0;
+                }
             }
 
-            print(markerPosition);
+            markerPosition.x = Mathf.Clamp(markerPosition.x, 15, Screen.width - 10);
+            markerPosition.y = Mathf.Clamp(markerPosition.y, 15, Screen.height - 15);
 
             GUI.Label( new Rect(markerPosition.x - 15, markerPosition.y - 15, markerPosition.x + 15, 30), marker);
         }
