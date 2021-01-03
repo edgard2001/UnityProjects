cca4be53e4df286aa0dd71cabc3b969a1b97dad2
edgard2001 <76737347+edgard2001@users.noreply.github.com>
2021-01-03 01:23:14 +0000

    Revert "Added cube to test git revert"
    This reverts commit b4b341cff4eedb697ade2db4fe4b93090522504e.

--- Space Game/Assets/Scripts/HUD/CursorScript.cs
+++ Space Game/Assets/Scripts/HUD/CursorScript.cs
@@ -11,8 +11,8 @@ public class CursorScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Confined;
+        //Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
