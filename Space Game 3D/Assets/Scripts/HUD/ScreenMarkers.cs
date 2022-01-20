1533c51e3ab5e5feca270dfc0e4032b15b7c287e
Edgard Romanovskiy <eromanovskiy303@gmail.com>
2022-01-20 20:47:13 +0000

    Added Space Game 3D.

---
+++ Space Game 3D/Assets/Scripts/HUD/ScreenMarkers.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenMarkers : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> enemies;
+    //[SerializeField] private float detectionRadius;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Texture marker;
+
+    private void OnGUI()
+    {
+        foreach (GameObject enemy in enemies)
+        {
+            Vector3 markerPosition = cam.WorldToScreenPoint(enemy.transform.position);
+            Vector3 displacement = enemy.transform.position - cam.transform.position;
+            float distance = displacement.magnitude;
+            string text = Mathf.RoundToInt(distance).ToString();
+
+            markerPosition.y = Screen.height - markerPosition.y;
+
+            if (markerPosition.z < 0)
+            {
+                if (markerPosition.x < Screen.width/2)
+                {
+                    markerPosition.x = Screen.width;
+                }
+                else
+                {
+                    markerPosition.x = 0;
+                }
+            }
+
+            markerPosition.x = Mathf.Clamp(markerPosition.x, 15, Screen.width - 10);
+            markerPosition.y = Mathf.Clamp(markerPosition.y, 15, Screen.height - 15);
+
+            GUI.Label( new Rect(markerPosition.x - 15, markerPosition.y - 15, markerPosition.x + 15, 30), marker);
+            GUI.Label(new Rect(markerPosition.x + 15, markerPosition.y - 15, markerPosition.x + 55, 30), text);
+        }
+    }
+
+
+
+}
